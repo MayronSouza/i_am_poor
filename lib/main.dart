9992c609340c12f02f8047a6dd7755b9f16d8e28
MayronGMZ <mayron.souza@gmz.solutions>
2020-06-05 21:59:44 -0300

    Inserindo imagem e finalizando App

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.orange[200],
           appBar: AppBar(
+            centerTitle: true,
             title: Text('I am Poor'),
-            backgroundColor: Colors.blue[300],
+            backgroundColor: Colors.orange[600],
           ),
           body: Center(
             child: Image(
-              image: AssetImage(''),
+              image: AssetImage('assets/images/a_poor_man.jpg'),
             ),
           ),
         ),
